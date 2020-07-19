78a4b1bc653605a3bc33be0408ad73ad5410fb74
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 14:11:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-alans_new_info="GUI"
-programmer_hash[:alans_kay][:known_for]=alans_new_info
-binding.pry
+programmer_hash[:alans_kay][:known_for]="GUI"
 programmer_hash
 
 end
