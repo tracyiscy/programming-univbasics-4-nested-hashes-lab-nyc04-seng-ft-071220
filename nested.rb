3b899e28f1141d0b6eb05269be558625f6aedb79
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 13:46:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,7 @@ def changing_alan
       }
     }
 programmer_hash[:known_for]=programmer_hash[:alans_new_info]
+programmer_hash
 end
 
 def adding_to_dennis
@@ -130,5 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+		programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		programmer_hash
 end
