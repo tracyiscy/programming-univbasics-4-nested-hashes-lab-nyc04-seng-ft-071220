6ff24dd2bff4dfb63d05a0bc7c036a73ba85a6e1
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 13:39:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash[:alan_kay][:known_for]
 
-	programmer_hash = 
+	programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
