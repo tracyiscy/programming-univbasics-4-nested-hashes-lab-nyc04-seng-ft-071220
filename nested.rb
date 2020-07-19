2ce9997ad90af16c1d4da11a6609aefa57f4b36d
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 14:08:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,5 @@
 
+require pry
 def hopper
 	programmer_hash =
  		{
@@ -109,6 +110,7 @@ def changing_alan
 alans_new_info="GUI"
 programmer_hash[:alans_kay][:known_for]=alans_new_info
 programmer_hash
+binding.pry
 
 end
 
